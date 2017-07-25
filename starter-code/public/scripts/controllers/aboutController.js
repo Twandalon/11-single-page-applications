eb7c084a3e6d1abfbb5f5af4553f0a3d7d4cf406
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-25 09:56:05 -0700

    Ready for turn in

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -7,7 +7,9 @@
   // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.init = function (){
     $('.tab-content').hide();
+    $('#articles').hide();
     $('#about').show();
+    console.log('about');
   }
 
   module.aboutController = aboutController;
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,5 +1,5 @@
 'use strict';
-// var app = app || {};
+var app = app || {};
 
 // DONE: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
 page('/', articleController.init);
